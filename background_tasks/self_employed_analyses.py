e65531ce3573af0a9f0d05c3efcd41d6c4afaf41
serj <sardnej@yandex.ru>
2019-09-29 00:24:20 +0300

    Анализ тортов и ногтей

--- background_tasks/self_employed_analyses.py
+++ background_tasks/self_employed_analyses.py
@@ -3,10 +3,34 @@ import os
 import urllib
 import sys
 import time
+import runpy as np
 sys.path.append('/home/serj/psb-server/')
 
 from flask import Flask
 from flask_mongoengine import MongoEngine
+
+from keras.models import load_model
+model = load_model("obj_gen_224_128_02_tl0.3169.h5")
+
+from PIL import Image
+IMAGE_SIZE = 128
+
+
+def img_as_np_array(file_name):
+    img = Image.open(file_name, 'r')
+    img = img.resize((IMAGE_SIZE, IMAGE_SIZE))
+    img = np.array(img) / 255. - 0.5
+    return img[:,:,:3]
+
+
+def load_imgs(folder):
+    imgs = []
+    files = os.listdir(folder)
+    for file in files:
+        img = img_as_np_array(f"{folder}/{file}")
+        imgs.append(img)
+    imgs = np.array(imgs)
+    return imgs
 
 from settings import MONGODB_SETTINGS
 
@@ -18,24 +42,58 @@ app.debug = True
 app.config['MONGODB_SETTINGS'] = MONGODB_SETTINGS
 db = MongoEngine(app)
 
-for new_request in VkUserRequest.objects(status="new"):
-    new_request.status = "in-progress"
-    # new_request.save()
-    posts = json.loads(new_request.data["posts"])
 
-    count = 0
-    for item in posts:
-        if 'attachments' in item:
-            attachments = item['attachments']
-            for attachment in attachments:
-                if attachment['type'] == 'photo':
-                    for size in attachment['photo']['sizes']:
-                        if size['type'] == 'q':
-                            url = size['url']
-                            folder = f"users/imgs_{new_request.vk_user_id}"
-                            if not os.path.isdir(folder):
-                                os.mkdir(folder)
-                            urllib.request.urlretrieve(url, f"{folder}/{count}")
-                            count += 1
-                            time.sleep(0.1)
-                            break
+def analys_request(new_request):
+    new_request.status = "in-progress"
+    new_request.save()
+    posts = json.loads(new_request.data["posts"])
+    folder = f"users/imgs_{new_request.vk_user_id}"
+    try:
+        count = 0
+        for item in posts:
+            if 'attachments' in item:
+                attachments = item['attachments']
+                for attachment in attachments:
+                    if attachment['type'] == 'photo':
+                        for size in attachment['photo']['sizes']:
+                            if size['type'] == 'q':
+                                url = size['url']
+                                if not os.path.isdir(folder):
+                                    os.mkdir(folder)
+                                urllib.request.urlretrieve(url, f"{folder}/{count}")
+                                count += 1
+                                time.sleep(0.1)
+                                break
+
+        imgs = load_imgs(folder)
+        preds = model.predict(imgs)
+
+        types = [0] * 3
+        for i, p in enumerate(preds):
+            types[np.argmax(p)] += 1
+
+        result = np.array(types) / sum(types)
+
+        if max(result[1], result[2]) > 0.01:
+            if result[2] > result[1]:
+                new_request.result = {
+                    "image_url": "https://sun9-8.userapi.com/c858416/v858416115/893ed/_z06T5vFG_o.jpg",
+                    "title": "Клиенты любят вашу выпечку",
+                    "subtitile": "Радуйте их, а о налогах позаботимся мы",
+                }
+            else:
+                new_request.result = {
+                    "image_url": "https://sun9-8.userapi.com/c858416/v858416115/893e3/0h4NZkHn1aI.jpg",
+                    "title": "Принимай клиентов легально",
+                    "subtitile": "Наши сервисы помогут организовать работу",
+                }
+
+            new_request.save()
+    except:
+        new_request.result["result"] = False
+        new_request.save()
+
+
+for new_request in VkUserRequest.objects(status="new"):
+    analys_request(new_request)
+
