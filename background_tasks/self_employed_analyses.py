e43ceb0ff525fc7c5d39cdc6c37bc2398f848d80
serj <sardnej@yandex.ru>
2019-09-29 05:34:20 +0300

    распознавание хат

--- background_tasks/self_employed_analyses.py
+++ background_tasks/self_employed_analyses.py
@@ -3,6 +3,7 @@ import os
 import urllib
 import sys
 import time
+import pickle
 import traceback
 import numpy as np
 sys.path.append('/home/serj/psb-server/')
@@ -36,6 +37,7 @@ def load_imgs(folder):
 from settings import MONGODB_SETTINGS
 
 from core.models import VkUserRequest
+from pymystem3 import Mystem
 from pprint import pprint
 
 app = Flask(__name__)
@@ -43,8 +45,40 @@ app.debug = True
 app.config['MONGODB_SETTINGS'] = MONGODB_SETTINGS
 db = MongoEngine(app)
 
+mystem = Mystem()
+
+
+def lemmatize_with_mystem(text):
+    words = [lemma for lemma in mystem.lemmatize(text) if not lemma.isspace() and not lemma.isnumeric()
+            and lemma.isalpha()]
+    return words
+
+
+def read_texts(file_names_in_classes, path_to_file = ""):
+    data = []
+    for class_index, per_classes in enumerate(file_names_in_classes):
+        for file_name in per_classes:
+            with open(f"{path_to_file}{file_name}") as file:
+                text = file.read()
+                asjson = json.loads(text)
+                if 'response' not in asjson:
+                    continue
+                items = asjson['response']['items']
+                for i in items:
+                    post = i['text']
+                    if len(post.strip()) == 0:
+                        continue
+                    data.append(post)
+
+    return data
+
 
 def analys_request(new_request):
+
+    with open('idf_vectorizer.pickle', 'rb') as f:
+        idf_vectorizer = pickle.load(f)
+    with open('logistic_regression.pickle', 'rb') as f:
+        log_regression = pickle.load(f)
 
     print("start analys_request")
     print("new_request")
@@ -58,6 +92,20 @@ def analys_request(new_request):
     if not os.path.isdir(folder):
         os.mkdir(folder)
     try:
+        for item in posts:
+            if "text" in item:
+                X = idf_vectorizer.transform(item["text"]).toarray()
+                predictions = log_regression.predict_proba(X)
+                if predictions[0][1] > 0.5:
+                    new_request.result = {
+                        "result": True,
+                        "image_url": "https://sun9-8.userapi.com/c858416/v858416115/893e3/0h4NZkHn1aI.jpg",
+                        "title": "Делай маникюр легально",
+                        "subtitile": "Наши сервисы помогут организовать работу",
+                    }
+                    new_request.save()
+                    return True
+
         count = 0
         for item in posts:
             if 'attachments' in item:
